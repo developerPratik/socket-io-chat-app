aefb19780e93ceba55ba59fd497a67c25db35a19
Pratik Manandhar <pratik.manandhar99@gmail.com>
2018-06-26 11:25:30 +0545

    Updated Login layouts

--- src/components/login.js
+++ src/components/login.js
@@ -66,37 +66,6 @@ export default class Login extends React.Component {
 
     render() {
 
-        if (1 !== 1) {
-            return (
-
-                <div className="wrapper fadeInDown">
-
-                    <div id="formContent">
-
-                        <h2 className="active"> Sign In </h2>
-                        <h2 className="inactive underlineHover">Sign Up </h2>
-
-
-                        <div className="fadeIn first">
-                            <img src="http://danielzawadzki.com/codepen/01/icon.svg" id="icon" alt="User Icon" />
-                        </div>
-
-
-                        <form onSubmit={this.login.bind(this)}>
-                            <input type="text" value={this.state.username} id="login" className="fadeIn second" name="username" placeholder="Username" onChange={(e) => { this.handleChange(e, 'username') }} />
-                            <input type="text" value={this.state.password} id="password" className="fadeIn third" name="password" placeholder="Password" onChange={(e) => { this.handleChange(e, 'password') }} />
-                            <input type="submit" className="fadeIn fourth" value="Log In" />
-                        </form>
-
-
-                        <div id="formFooter">
-                            <a className="underlineHover" href="#">Forgot Password?</a>
-                        </div>
-
-                    </div>
-                </div>);
-        }
-
         return (
             <div className="cont_login_wrapper">
                 <div className="cont_principal">
